b0311ff1e431320dc56fb779e8d57304f062fac1
Tristen Pool <tristenpool89@gmail.com>
2021-07-24 12:31:08 -0600

    remove redundant file deletion feature

--- src/main.py
+++ src/main.py
@@ -7,11 +7,3 @@ from core import core
 
 assistant = core()
 assistant.run()
-
-# delete temporary audio files after running
-dir_list = os.listdir("../Asclepius")
-
-for file in dir_list:
-    if file[-4:] == ".wav" or file[-4:] == ".mp3":
-        os.remove(file)
-        
