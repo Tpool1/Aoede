de36a810caefb40d855d760abf65f8afcd4981cf
Tristen Pool <tristenpool89@gmail.com>
2021-07-30 10:47:45 -0500

    add error handling to voice_to_date

--- src/packages/voice_to_date.py
+++ src/packages/voice_to_date.py
@@ -1,16 +1,24 @@
 from urllib.parse import urldefrag
 from packages.get_parsed_input import get_parsed_input
 from packages.txt_num_dicts import month_dict
+from play_text import play_text
 import re
 
 # function to convert a date that is said verbally to numerical format (year-month-day) (Ex. 2021-07-23)
 
 def voice_to_date():
-    user_input = get_parsed_input()
+    
+    date_received = False
+    while not date_received:
+        user_input = get_parsed_input()
+        try:
+            year = user_input['CD2']
+            month = user_input['NNP']
+            day = user_input['CD']
+            date_received = True
+        except KeyError:
+            play_text('I could not identify this date. Please try again.')
 
-    year = user_input['CD2']
-    month = user_input['NNP']
-    day = user_input['CD']
 
     day = re.sub("[^0-9]", "", day)
     
