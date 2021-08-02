44a007562d7a817a14e94d96ccb14eaf5abe75c3
Tristen Pool <tristenpool89@gmail.com>
2021-08-02 11:41:25 -0500

    start setup file

---
+++ setup.py
@@ -0,0 +1,4 @@
+from src.play_text import play_text
+from src.packages.get_parsed_input import get_parsed_input
+
+play_text('Hi! My name is Asclepius. Let me ask you some questions to get to know you then we will be set.')
