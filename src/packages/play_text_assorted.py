74813e96f85e35d7fd84212abec4d19c6aa91623
Tristen Pool <tristenpool89@gmail.com>
2021-08-02 12:51:51 -0500

    fix spacing in play_text_assorted

--- src/packages/play_text_assorted.py
+++ src/packages/play_text_assorted.py
@@ -2,6 +2,7 @@ from play_text import play_text
 from packages.partition import partition
 from packages.filterStops import filterStops
 from packages.stem import stem
+from packages.tag import tag
 from packages.get_word_synonyms import get_word_synonyms
 
 import random
@@ -9,9 +10,11 @@ import random
 # function to play a set of text with some words ambiguously replaced with synonyms as to make the speech more natural
 
 def play_text_assorted(text):
+
     original_text_list = partition(text)
     word_list = filterStops(original_text_list)
     word_list = stem(word_list)
+    tag_dict = tag(word_list)
 
     # dictionary to store original words with the synonyms chosen
     word_syn_dict = {}
@@ -27,26 +30,26 @@ def play_text_assorted(text):
             pass
 
     i = 0
+    text_list = []
     for sent in original_text_list:
         j = 0 
         for word in sent:
             if word in list(word_syn_dict.keys()):
-                replacement = word_syn_dict[word]
-                sent[j] = replacement
+                if word in list(tag_dict.values()):
+                    replacement = word_syn_dict[word]
+                    sent[j] = replacement
 
             j = j + 1
 
-        original_text_list[i] = sent
+        text_list.append(sent)
 
         i = i + 1
 
-    generated_text = ''
-    print(original_text_list)
-    for sent in original_text_list:
-        generated_text = generated_text.join(sent)
-
-    print(generated_text)
-
+    for sent in text_list:
+        it = map(str, sent)
+        generated_text = next(it, '')
+        for s in it:
+            generated_text += ' ' + s
+    
     play_text(generated_text)
     
-
