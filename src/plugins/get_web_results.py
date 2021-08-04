db7455a9daf70f37b14a303990010ded684208e9
Tristen Pool <tristenpool89@gmail.com>
2021-08-04 08:58:42 -0600

    add get_web_results plugin

---
+++ src/plugins/get_web_results.py
@@ -0,0 +1,14 @@
+from googlesearch import search
+from packages.get_parsed_input import get_parsed_input
+from play_text import play_text
+
+def get_web_results():
+    
+    play_text('What would you like to search for today?')
+    query = get_parsed_input()
+
+    url_list = []
+    for result in search(query, tld="co.in", num=5, stop=5, pause=2):
+        url_list.append(result)
+
+    print(url_list)
