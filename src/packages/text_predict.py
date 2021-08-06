1fcc5673a24b3bdf1a8d8434e6b6b6ae2af72bb7
Tristen Pool <tristenpool89@gmail.com>
2021-08-06 11:44:05 -0600

    add keras model

--- src/packages/text_predict.py
+++ src/packages/text_predict.py
@@ -1,6 +1,10 @@
 import os
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
 from tensorflow.keras.preprocessing.text import Tokenizer
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 # number of words model with use for training data
 word_num = 6
@@ -15,7 +19,6 @@ for file in file_list:
     file_list[i] = full_path
     i = i + 1
 
-
 files = []
 for path in file_list:
     file = open(path, 'r').read()
@@ -29,8 +32,8 @@ data = tokenizer.texts_to_sequences(files)
 
 data = np.asarray(data, dtype=object)
 
-x = np.array([])
-y = np.array([])
+x = np.empty(shape=(852250, 6), dtype=int)
+y = np.empty(shape=(852250,), dtype=int)
 
 i = 0
 j = word_num
@@ -39,15 +42,30 @@ for text in data:
 
         x_single = text[i:j]
 
-        x = np.append(x, x_single)
+        x[i:j] = x_single
 
         y_single = text[j]
 
-        y = np.append(y, y_single)
+        y[i] = y_single
 
         i = i + 1
         j = j + 1
 
-    print(x.shape)
-    print(y.shape)
-        
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+input = keras.Input(shape=(6))
+
+x = layers.Dense(6, activation='relu')(input)
+x = layers.Dense(3, activation='relu')(x)
+
+output = layers.Dense(1, activation='linear')(x)
+
+model = keras.Model(inputs=input, outputs=output)
+
+model.compile(optimizer='sgd', loss='mean_squared_error', metrics=['accuracy'])
+
+model.fit(x_train, y_train, batch_size=16, epochs=12)
+
+model.save('keras_word_prediction_model.h5')
+
+model.evaluate(x_test, y_test)
