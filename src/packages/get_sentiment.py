c811f2eb16b4a54263f51fb48b239e1b2aa8f65c
Tristen Pool <tristenpool89@gmail.com>
2021-08-13 10:44:20 -0500

    start sentiment finder

---
+++ src/packages/get_sentiment.py
@@ -0,0 +1,7 @@
+import os
+from keras_preprocessing import text
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.preprocessing.text import Tokenizer
+import numpy as np
+from sklearn.model_selection import train_test_split
