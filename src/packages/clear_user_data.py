4d8a799abc5cef5c2c71d7b1ae20b9ff51d0fd9b
Tristen Pool <tristenpool89@gmail.com>
2021-08-22 20:57:28 -0500

    update clear_user_data

--- src/packages/clear_user_data.py
+++ src/packages/clear_user_data.py
@@ -1,6 +1,14 @@
+import os
 
 def clear_user_data():
-    f = open("data\\user_data\\conversations.txt", "r+")
-    f.truncate(0)
-    f.close()
-    
+    root = "data\\profiles"
+    profile_list = os.listdir(root)
+
+    for profile in profile_list:
+        profile_path = os.path.join(root, profile)
+        data_list = os.listdir(profile_path)
+
+        for data in data_list:
+            data_path = os.path.join(profile_path, data)
+            os.remove(data_path)
+            
