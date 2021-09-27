9d910cba74518e99a9f6e570fa7ba1c02f7610be
Tristen Pool <tristenpool89@gmail.com>
2021-09-27 18:54:53 -0500

    add test and val functionality

--- src/models/cancer_ml/clinical_only.py
+++ src/models/cancer_ml/clinical_only.py
@@ -6,8 +6,8 @@ class clinical_only:
 
     def __init__(self, load_model=True):
         self.load_model = load_model
-    
-    def train_model(self, X_train, y_train, epochs=10, batch_size=32):
+
+    def train_model(self, X_train, y_train, X_val, y_val, epochs=10, batch_size=32):
         input = keras.layers.Input(shape=(self.X_train.shape[1],))
 
         x = Dense(9, activation=self.activation_function)(input)
@@ -22,17 +22,22 @@ class clinical_only:
                             loss='mean_squared_error',
                             metrics=['accuracy', MeanIoU(num_classes=2)])
 
-        model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size)
+        model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_data=(X_val, y_val))
 
         model.save('data\\saved_models\\text_prediction\\keras_clinical_only_model.h5')
 
         return model
 
-    def get_model(self, X_train=None, y_train=None, epochs=None, batch_size=None):
+    def test_model(self, X_test, y_test):
+        results = self.model.evaluate(X_test, y_test, batch_size=128)
+
+        return results
+
+    def get_model(self, X_train=None, y_train=None, X_val=None, y_val=None, epochs=None, batch_size=None):
         
         if self.load_model:
             self.model = keras.models.load_model('data\\saved_models\\keras_clinical_only_model.h5')
         else:
-            self.model = self.train_model(X_train, y_train, epochs, batch_size)
+            self.model = self.train_model(X_train, y_train, X_val, y_val, epochs, batch_size)
 
         return self.model
--- src/models/cancer_ml/image_model.py
+++ src/models/cancer_ml/image_model.py
@@ -7,7 +7,7 @@ class image_model:
     def __init__(self, load_model=True):
         self.load_model = load_model
 
-    def train_model(self, X_train, y_train, epochs=10, batch_size=32):
+    def train_model(self, X_train, y_train, X_val, y_val, epochs=10, batch_size=32):
         input = keras.layers.Input(shape=(self.X_train.shape[1],))
 
         x = Dense(150, activation=self.activation_function)(input)
@@ -27,18 +27,23 @@ class image_model:
                             loss='mse',
                             metrics=['accuracy'])
 
-        self.fit = model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size)
+        self.fit = model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_data=(X_val, y_val))
 
         model.save('data\\saved_models\\text_prediction\\keras_image_clinical_model.h5')
 
         return model
 
-    def get_model(self, X_train=None, y_train=None, epochs=None, batch_size=None):
+    def test_model(self, X_test, y_test):
+        results = self.model.evaluate(X_test, y_test, batch_size=128)
+
+        return results
+
+    def get_model(self, X_train=None, y_train=None, X_val=None, y_val=None, epochs=None, batch_size=None):
         
         if self.load_model:
             self.model = keras.models.load_model('data\\saved_models\\text_prediction\\keras_image_clinical_model.h5')
         else:
-            self.model = self.train_model(X_train, y_train, epochs, batch_size)
+            self.model = self.train_model(X_train, y_train, X_val, y_val, epochs, batch_size)
 
         return self.model
         
