26595cb31bff13472633f93d1e8b7a30cc393513
Tristen Pool <tristenpool89@gmail.com>
2022-03-29 16:13:20 -0500

    add function buttons without voice

--- src/packages/play_text.py
+++ src/packages/play_text.py
@@ -2,6 +2,7 @@ import pyttsx3
 from packages.write_conversation_data import write_conversation_data
 
 def play_text(text):
+
     engine = pyttsx3.init()
     engine.say(text)
     engine.runAndWait()
